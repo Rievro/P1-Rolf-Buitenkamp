5a5fc30890eec8cb75c41093872e510d8520623f
Rievro <buitenkamprolf@gmail.com>
2026-09-10 11:29:42 +0200

    week 1 opdrachten
    week 1 mondriaan, mijn naam - mario

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -1,7 +1,152 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1000, 1000);
 }
 
 function draw() {
-  background(220);
+  background(111);
+  textSize(32);
+  fill('black');
+  text('Rolf Buitenkamp', 10, 30);
+  text('1', 10, 60);
+  text('2', 10, 165);
+  text('3', 50, 350);
+  text('4', 50, 565);
+  text('5', 350, 30);
+  text('6', 350, 500);
+  text('7 mario', 650, 230);
+   fill('white');
+  rect(40, 100, 200, 100);
+  square(100, 300, 100); 
+  fill('red');
+  ellipse(137, 150, 50, 50);
+
+  fill('black');
+  square(100, 300, 20);
+  square(140, 300, 20);
+  square(180, 300, 20);
+  square(100, 340, 20);
+  square(100, 380, 20);
+  square(120, 320, 20);
+  square(120, 360, 20);
+  square(160, 320, 20);
+  square(160, 360, 20);
+  square(180, 380, 20);
+  square(180, 340, 20);
+  square(140, 380, 20);
+  square(140, 340, 20);
+
+fill('red');
+  square(100, 500, 100);
+  fill ('blue')
+  triangle(100, 500, 150, 450, 200, 500);
+
+  fill('lightgrey');
+  rect(350, 50, 100, 350);
+  rect(375, 400, 50, 50)
+  fill('red');
+  ellipse(400, 100, 50, 50);
+  fill ('green');
+  ellipse(400, 300, 50, 50);
+  fill ('orange');
+  ellipse(400, 200, 50, 50);
+
+fill('black');
+  rect(350, 500, 100, 100, 15);
+  fill('white');
+  rect(360, 510, 80, 80, 5);
+  fill('black');
+  ellipse(400, 550, 15, 15);  
+  ellipse(425, 575, 15, 15);
+  ellipse(375, 525, 15, 15);
+  ellipse(425, 525, 15, 15);
+  ellipse(375, 575, 15, 15);
+
+ fill('red');
+ square(650, 250, 25);
+square(675, 250, 25);
+square(700, 250, 25);
+square(725, 250, 25);
+square(750, 250, 25);
+square(650, 275, 25);
+square(625, 275, 25);
+square(750, 275, 25);
+square(775, 275, 25);
+fill('white');
+square(675, 275, 25);
+square(700, 275, 25);
+square(725, 275, 25); 
+fill('red')
+square(625, 300, 25);
+square(650, 300, 25);
+square(675, 300, 25);
+square(700, 300, 25);
+square(725, 300, 25);
+square(750, 300, 25);
+square(775, 300, 25);
+fill('brown');
+square(625, 325, 25);
+square(775, 325, 25);
+fill('black');
+square(675, 325, 25);
+square (725, 325, 25);
+fill('coral');
+square(650, 325, 25);
+square(700, 325, 25);
+square(750, 325, 25);
+square(625, 350, 25);
+square(650, 350, 25);
+fill('brown');
+square(675, 350, 25);
+square(700, 350, 25);
+square(725, 350, 25);
+fill('coral')
+square(750, 350, 25);
+square(775, 350, 25);
+square(650, 375, 25);
+square(675, 375, 25);
+square(700, 375, 25);
+square(725, 375, 25);
+square(750, 375, 25);
+fill('blue');
+square(650, 400, 25);
+square(750, 400, 25);
+square(675, 425, 25);
+square(725, 425, 25);
+square(700, 425, 25);
+square(675, 450, 25);
+square(700, 450, 25);
+square(725, 450, 25);
+square(750, 450, 25);
+square(650, 450, 25);
+square(650, 475, 25);
+square(675, 475, 25);
+square(700, 475, 25);
+square(725, 475, 25);
+square(750, 475, 25);
+fill('brown');
+square(650, 500, 25);
+square(675, 500, 25);
+square(725, 500, 25);
+square(750, 500, 25);
+fill('yellow');
+square(650, 425, 25);
+square(750, 425, 25);
+fill('red');
+square(675, 400, 25);
+square(700, 400, 25);
+square(725, 400, 25);
+square (625, 400, 25);
+square(775, 400, 25);
+square(625, 425, 25);
+square(775, 425, 25);
+fill('white');
+square(625, 450, 25);
+square(775, 450, 25);
+
+
+
 }
+
+
+
+
