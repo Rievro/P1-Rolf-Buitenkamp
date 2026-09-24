4c18a570c196eb3f3f6bdfa588e2e9f5122bc584
Rievro <buitenkamprolf@gmail.com>
2026-09-24 15:31:51 +0200

    E

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,7 +1,7 @@
 let currentPlayer = 1 // 1 red, 2 blue
-let gamestate = 'playing'
+let gameState = 'playing'
 let board = [0, 0, 0, 0, 0, 0, 0, 0, 0] // 0 = not clicked, 1 = red, 2 = blue
-let game_over = false
+let winPlayer = 0;
 let winner = 0;
 
 
@@ -17,8 +17,8 @@ function drawSquare(x, y, index) {
 } // new function for squares to make it less cluttered
 
 function mousePressed() {
-  if (gamestate !== 'playing') return;
- // mouse locations === square locations checks // 
+  if (gameState !== 'playing') return;
+  // mouse locations === square locations checks // 
   if (mouseX > 200 && mouseX < 320 && mouseY > 200 && mouseY < 320 && board[0] === 0) {
     board[0] = currentPlayer;
     if (currentPlayer === 1) {
@@ -42,7 +42,8 @@ function mousePressed() {
     }
   } else if (mouseX > 350 && mouseX < 470 && mouseY > 200 && mouseY < 320 && board[3] === 0) {
     board[3] = currentPlayer;
-    if (currentPlayer === 1) {  currentPlayer = 2;
+    if (currentPlayer === 1) {
+      currentPlayer = 2;
     } else {
       currentPlayer = 1;
     }
@@ -76,21 +77,57 @@ function mousePressed() {
     }
   } else if (mouseX > 500 && mouseX < 620 && mouseY > 500 && mouseY < 620 && board[8] === 0) {
     board[8] = currentPlayer;
-    if (currentPlayer === 1) {  currentPlayer = 2;
+    if (currentPlayer === 1) {
+      currentPlayer = 2;
     } else {
       currentPlayer = 1;
     }
   }
   console.log(board)
 
-  if(board[0] == board[3] && board[3] == board[6] && board [0] !== 0) {
- console.log('winner')
+  if (board[0] == board[3] && board[3] == board[6] && board[0] !== 0) {
+    console.log('winner1')
+    gameState = 'won'
+    winPlayer = board[0]
   }
-  if(board[1] == board[2] && board[2] == board[4] && board [0] !== 0) {
- console.log('winner')
+  if (board[1] == board[2] && board[2] == board[4] && board[1] !== 0) {
+    console.log('winner2')
+    gameState = 'won'
+    winPlayer = board[1]
   }
-  if(board[5] == board[7] && board[7] == board[8] && board [0] !== 0) {
- console.log('winner')
+  if (board[5] == board[7] && board[7] == board[8] && board[5] !== 0) {
+    console.log('winner3')
+    winPlayer = board[5]
+    gameState = 'won'
+  }
+  if (board[0] == board[1] && board[1] == board[7] && board[0] !== 0) {
+    console.log('winner4')
+    gameState = 'won'
+    winPlayer = board[0]
+  }
+  if (board[2] == board[3] && board[3] == board[5] && board[2] !== 0) {
+    console.log('winner5')
+    gameState = 'won'
+    winPlayer = board[2]
+  }
+  if (board[4] == board[6] && board[6] == board[8] && board[4] !== 0) {
+    console.log('winner6')
+    gameState = 'won'
+    winPlayer = board[4]
+  }
+  if (board[0] == board[2] && board[2] == board[8] && board[0] !== 0) {
+    console.log('winner7')
+    gameState = 'won'
+    winPlayer = board[0]
+  }
+  if (board[2] == board[6] && board[6] == board[7] && board[2] !== 0) {
+    console.log('winner8')
+    gameState = 'won'
+    winPlayer = board[2]
+  }
+  if (board[0] !== 0 && board[1] !== 0 && board[2] !== 0 && board[3] !== 0 && board[4] !== 0 && board[5] !== 0 && board[6] !== 0 && board[7] !== 0 && board[8] !== 0 && winPlayer === 0) {
+    gameState = 'draw'
+    winPlayer = 'draw'
   }
 }
 
@@ -107,7 +144,7 @@ function draw() {
   square(195, 190, 430, 5);
   strokeWeight(10);
 
- //squares
+  //squares
 
   drawSquare(200, 200, 0); // linksboven 
   drawSquare(200, 350, 1); // linksmidden
@@ -119,8 +156,22 @@ function draw() {
   drawSquare(200, 500, 7); // linksonder
   drawSquare(500, 500, 8); // rechtsonder
 
- 
+  if (gameState === 'won') {
+    textSize(40)
+    fill('darkgreen')
+    text('player ' + winPlayer + ' has won!', 250, 150)
+
+  }
+  if (winPlayer === 'draw') {
+    textSize(40)
+    fill('blue')
+    text('Its a draw', 250, 150)
+    console.log('draw')
+  }
+
+
+
+
+
 }
-
-
- 
+ff
